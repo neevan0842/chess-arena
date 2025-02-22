df259fdefdbcba4d374bc889337945bbf951931a
Naveen Manoj <manojnaveen085@gmail.com>
2025-02-22 19:09:05 +0530

    After shadcn and tailwind to 3

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      Login
+      <Button>Login</Button>
+    </div>
+  );
 };
 
 export default Login;
